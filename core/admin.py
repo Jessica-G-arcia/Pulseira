c2937bfa638852367c1bff9439055cebf8fa67d2
Jessica-G-arcia <jeh.garcia.jg@gmail.com>
2026-01-10 15:13:00 -0300

    convenio

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,34 @@
 from django.contrib import admin
 from .models import Pulseira
 
-admin.site.register(Pulseira)
+@admin.register(Pulseira)
+class PulseiraAdmin(admin.ModelAdmin):
+    # Colunas que aparecem na lista de pulseiras
+    list_display = ('nome', 'idade_visual', 'tipo_sanguineo', 'responsavel_telefone')
+    search_fields = ('nome', 'responsavel_nome')
+    
+    # Organização visual do formulário (Grupos)
+    fieldsets = (
+        ('Identificação', {
+            'fields': ('nome', 'nascimento', 'foto', 'tipo_sanguineo', 'aceite_termos')
+        }),
+        ('Convênio e SUS', {
+            'fields': ('possui_convenio', 'nome_convenio', 'numero_sus_convenio'),
+            'description': 'Selecione SIM no convênio para digitar o nome.'
+        }),
+        ('Saúde (Opcional)', {
+            'fields': ('condicao_medica', 'alergias', 'medicamentos'),
+            'classes': ('collapse',), # Deixa essa parte "encolhida" para não poluir, clique para abrir
+        }),
+        ('Instruções', {
+            'fields': ('instrucoes_abordagem',)
+        }),
+        ('Contato de Emergência', {
+            'fields': ('responsavel_nome', 'responsavel_telefone', 'responsavel_email')
+        }),
+    )
+
+    # Função auxiliar para mostrar idade na lista
+    def idade_visual(self, obj):
+        return f"{obj.idade} anos"
+    idade_visual.short_description = "Idade"
