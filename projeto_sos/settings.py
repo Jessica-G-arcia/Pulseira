9821adc6e447b6f0a3b8775750f513654d6d5812
Jessica-G-arcia <jeh.garcia.jg@gmail.com>
2026-01-10 23:02:36 -0300

    banco

--- projeto_sos/settings.py
+++ projeto_sos/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'projeto_sos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'JessicaGarcia1$sos',       # Nome que apareceu na aba Databases
+        'USER': 'JessicaGarcia1',           # Seu usuário do PythonAnywhere
+        'PASSWORD': 'SUA_SENHA_DO_MYSQL',   # A senha que você criou na aba Databases
+        'HOST': 'JessicaGarcia1.mysql.pythonanywhere-services.com', # O endereço do Host
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            # Isso é CRUCIAL no PythonAnywhere para a conexão não cair:
+            'charset': 'utf8mb4',
+        },
     }
 }
+
+CONN_MAX_AGE = 300
 
 AUTHENTICATION_BACKENDS = [
     'core.auth_backends.CPFOrEmailBackend', # Seu novo login
